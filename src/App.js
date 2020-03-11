6a7962bf953d9450009d2fb0d6a0cb03e5dd2ec5
Bashir <b_egeh@hotmail.com>
2020-03-11 09:55:51 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,79 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import axios from 'axios';
+
 import logo from './logo.svg';
 import './App.css';
+import Typography from '@material-ui/core/Typography';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
 
-function App() {
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+export default function App() {
   return (
+    <Router>
+      
+      <body>
+        
+        <Switch>
+          <Route exact path="/">
+            <SignIn />
+          </Route>
+          <Route path="/legacy">
+            <Legacy />
+          </Route>
+          <Route path="/hall">
+            <Hall />
+          </Route>
+          {/* <Route path="/lobby">
+            <Lobby />
+          </Route> */}
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+        </Switch>
+      </body>
+    </Router>
+  );
+}
+
+function Legacy() {
+  return (
+    <>
+    <NavForApp />
+    <div>
+      <h2>Legacy</h2>
+    </div>
+    </>
+  );
+}
+
+function Lobby(){
+  return(
+    <>
+    <NavForApp />
+    <div>
+      <h2>Lobby</h2>
+    </div>
+    </>
+  )
+}
+
+function Hall() {
+  axios.get('http://localhost:8001/api/appointments')
+  .then(function (response) {
+    console.log(response.data);
+  })
+  return (
+    <>
+    <NavForApp />
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,7 +90,31 @@ function App() {
         </a>
       </header>
     </div>
+    </>
   );
 }
 
-export default App;
+function NavForApp(){
+
+  return(
+    <Typography>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/hall">LEVEL-UP</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/legacy">Legacy</Nav.Link>
+            <Nav.Link href="/hall">Hall</Nav.Link>
+          </Nav>
+          <Nav className="ml-auto">
+            <Nav.Link href="/">Logout</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+    <hr />
+  </Typography>
+  )
+  }
+
+
+  
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min.js'
+import 'typeface-roboto'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
