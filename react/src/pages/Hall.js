54d7e8cae073c2b338c546ffd96652b1cd052765
ChristoLuksatrio <christo.luksatrio@gmail.com>
2020-03-12 17:04:01 -0400

    added quest, hall, post skeleton template

---
+++ react/src/pages/Hall.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+
+import NavForApp from '../components/NavForApp';
+
+
+
+const HallList = ({quests}) => {
+  return (
+    <div>
+      {
+        quests.map((quest, index) => (
+          <HallListItem title={quest.title} nodes={quest.nodes} index={index} />
+        ))
+      }
+
+      <button>New Quest</button>
+
+    </div>
+  )
+}
+
+const HallListItem = ({title, nodes}) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{nodes.length}</p>
+    </div>
+  )
+}
+
+export default function Hall() {
+  
+
+
+
+  return (
+    <>
+    <NavForApp nav_title='HALL' />
+    <Grid container >
+      <Grid item xs={false} sm={6} md={6} >
+        <p>Hello</p>
+      </Grid>
+
+      <Grid item xs={12} sm={6} md={6} >
+        <HallList quests={quests} />
+      </Grid>
+    </Grid>
+
+
+    </>
+  );
+}
+
+
+
+
+
+const quests = [
+  {
+    title: 'quest1',
+    nodes: [
+      {
+        title: 'start',
+        isComplete: true
+      },
+      {
+        title: 'middle',
+        isComplete: true
+      },
+      {
+        title: 'end',
+        isComplete: false
+      },
+    ]
+  },
+  {
+    title: 'quest1',
+    nodes: [
+      {
+        title: 'start',
+        isComplete: true
+      },
+      {
+        title: 'middle',
+        isComplete: true
+      },
+      {
+        title: 'end',
+        isComplete: false
+      },
+    ]
+  },
+  {
+    title: 'quest1',
+    nodes: [
+      {
+        title: 'start',
+        isComplete: true
+      },
+      {
+        title: 'middle',
+        isComplete: true
+      },
+      {
+        title: 'end',
+        isComplete: false
+      },
+    ]
+  }
+]
---
+++ react/src/pages/Post.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import NavForApp from '../components/NavForApp';
+
+
+export default function Post() {
+
+  const post = {
+    title: 'quest',
+    date: Date(Date.now()).toString(),
+    description: "This is my progress",
+    symbol: "https://cdn4.iconfinder.com/data/icons/must-have-outline/100/objects-29-512.png", 
+    attachment: "https://cdn.theatlantic.com/thumbor/ZAWCcyd-MwxmwvkTGp9VtFjb-h0=/900x673/media/img/photo/2018/11/photos-companionable-capybaras/c02_142762210/original.jpg",
+    comments: [
+      {
+        username: "Bash",
+        avatar: "https://ih0.redbubble.net/image.539207575.3366/flat,128x128,075,t-pad,128x128,f8f8f8.u2.jpg",
+        date: Date(Date.now()).toString(),
+        text: "Capybaras are awesome"
+      },
+      {
+        username: "Bash",
+        avatar: "https://ih0.redbubble.net/image.539207575.3366/flat,128x128,075,t-pad,128x128,f8f8f8.u2.jpg",
+        date: Date(Date.now()).toString(),
+        text: "Capybaras are awesome"
+      },
+      {
+        username: "Bash",
+        avatar: "https://ih0.redbubble.net/image.539207575.3366/flat,128x128,075,t-pad,128x128,f8f8f8.u2.jpg",
+        date: Date(Date.now()).toString(),
+        text: "Capybaras are awesome"
+      }
+    ]
+  }
+
+  const PostView = ({post}) => {
+    return (
+      <div>
+        <img src={post.symbol} />
+        <h1>{post.title}</h1>
+        <h2>{post.date}</h2>
+        <p>{post.description}</p>
+        <img src={post.attachment} />
+        <form>
+          <input placeholder="add comment" />
+          <button>Submit</button>
+        </form>
+        
+      </div>
+    )
+  }
+
+  const CommentList = ({post}) => {
+
+    return (
+      post.comments.map((comment, index) => (
+        <CommentListItem key={index} username={comment.username} avatar={comment.avatar} date={comment.date} text={comment.text} />
+      ))
+    )
+  }
+
+  const CommentListItem = ({ username, avatar, date, text }) => {
+    return (
+      <div>
+        <p>{username}</p>
+        <img src={avatar} />
+        <p>{date}</p>
+        <p>{text}</p>
+      </div>
+    )
+  }
+
+
+
+  return (
+    <>
+    <NavForApp nav_title='POST' />
+    <Grid container >
+      <Grid item xs>
+        <p>Hello</p>
+      </Grid>
+
+      <Grid item xs >
+        <PostView post={post} />
+        <CommentList post={post} />
+      </Grid>
+    </Grid>
+    </>
+  );
+}
