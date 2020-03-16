103e85095c4061d9ea5f85e6db452dccdf83c8ac
Bashir <b_egeh@hotmail.com>
2020-03-16 19:18:29 -0400

    users quests passed to legacy page and loaded

--- react/src/components/NavForApp.js
+++ react/src/components/NavForApp.js
@@ -6,10 +6,24 @@ import PartyBtn from './PartyBtn';
 import QuestInfoBtn from './QuestInfoBtn';
 import { useHistory } from "react-router-dom";
 import './NavForApp.css'
+import axios from 'axios';
 
 export default function NavForApp(props){
   let history = useHistory();
   const state = props.state
+
+  function loadQuests(){
+    return axios.get("/quests")
+    .then((res)=>{
+      const yourQuests = res.data.filter(quest => quest.user_id === state.id);
+      console.log(`Your quests ${JSON.stringify(yourQuests)}`)
+      return yourQuests;
+    })
+    .then((res)=>{
+      history.push({pathname:"/legacy",state:{global: state, quests: res}});
+    });
+  }
+
   return(
     <Typography>
     <Navbar bg="light" expand="lg">
@@ -20,7 +34,7 @@ export default function NavForApp(props){
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link onClick={()=>history.push({pathname:"/legacy",state:state})}>Legacy</Nav.Link>
+            <Nav.Link onClick={loadQuests}>Legacy</Nav.Link>
             <Nav.Link onClick={()=>history.push({pathname: "/hall", state: state})}>Hall</Nav.Link>
             <Nav.Link onClick={()=>history.push("/")}
             >Logout</Nav.Link>
