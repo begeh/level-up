c0714a5a3d014cfe03f6c193ec40dbdb718bfe82
ChristoLuksatrio <christo.luksatrio@gmail.com>
2020-03-17 11:19:49 -0400

    update

--- rails-server/db/schema.rb
+++ rails-server/db/schema.rb
@@ -10,10 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_03_15_214244) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
 
   create_table "comments", force: :cascade do |t|
     t.string "text"
