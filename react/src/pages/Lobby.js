6e990c08281ecb6e9a8e31ec774f1d7a8e7d63f2
Stoneface-Vimes <cliffweir92@gmail.com>
2020-03-17 18:08:59 -0400

    Working on splitting the handleSubmit function

--- react/src/pages/Lobby.js
+++ react/src/pages/Lobby.js
@@ -1,4 +1,4 @@
-import React, {useState, Fragment} from 'react';
+import React, { useState, Fragment } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
@@ -33,27 +33,30 @@ const useStyles = makeStyles(theme => ({
 
 export default function Lobby(props) {
   const state = props.location.state;
-  let quests ={};
+  let quests = {};
   console.log(`New State: ${state}`)
   console.log(`Received email: ${props.location.state.email} and password: ${props.location.state.password}`);
 
   const classes = useStyles();
   let history = useHistory();
-  const [lobbyName, setLobbyName] = useState(null);
-  const [lobbyCode, setLobbyCode] = useState(null);
+  const [lobbyName, setLobbyName] = useState(null); //Create lobby
+  const [lobbyCode, setLobbyCode] = useState(null); //Join Lobby
 
   async function handleSubmit(event) {
     event.preventDefault();
-    console.log(`lobbyname is ${lobbyName}`);
-    console.log(`lobbycode is ${lobbyCode}`);
-    if(lobbyName && !lobbyCode){
+    console.log(`lobbyname is ${lobbyName}`); //Create lobby
+    console.log(`lobbycode is ${lobbyCode}`); //Join Lobby
+    
+    // If making Lobby
+    if (lobbyName && !lobbyCode) {
       state.lobbyName = lobbyName
-    } else if(lobbyCode && !lobbyName){
+    // If joining lobby
+    } else if (lobbyCode && !lobbyName) {
       state.lobbyCode = lobbyCode
-    } 
+    }
 
     let quests = await axios.get("/quests")
-      .then((res)=>{
+      .then((res) => {
         const yourQuests = res.data.filter(quest => quest.user_id === state.id);
         console.log(`Your quests ${JSON.stringify(yourQuests)}`)
         return yourQuests;
@@ -61,103 +64,103 @@ export default function Lobby(props) {
 
     let full_quests = [];
     let promises = [];
-    quests.forEach((quest)=>{
+    quests.forEach((quest) => {
       promises.push(axios.get(`quest_object/${quest.id}`)
-      .then((response)=>{
-        full_quests.push(response.data);
-      })
+        .then((response) => {
+          full_quests.push(response.data);
+        })
       )
-      }
+    }
     );
 
     await Promise.all(promises);
 
     let party_quests = await axios.get("/quests")
-    .then((res)=>{
-      const partyQuests = res.data.filter(quest => quest.party_id === state.party_id && quest.status === "underway");
-      return partyQuests;
-    })
+      .then((res) => {
+        const partyQuests = res.data.filter(quest => quest.party_id === state.party_id && quest.status === "underway");
+        return partyQuests;
+      })
 
     let party_full_quests = [];
     let party_promises = [];
-    party_quests.forEach((quest)=>{
+    party_quests.forEach((quest) => {
       party_promises.push(axios.get(`quest_object/${quest.id}`)
-      .then((response)=>{
-        party_full_quests.push(response.data);
-      })
+        .then((response) => {
+          party_full_quests.push(response.data);
+        })
       )
-      }
+    }
     );
 
     await Promise.all(party_promises);
-    
+
     console.log(`Party full quests ${JSON.stringify(party_full_quests)}`);
 
-    
-    history.push({pathname:"/hall",state:{global: state, quests: full_quests, party_quests: party_full_quests}});
-    
+
+    history.push({ pathname: "/hall", state: { global: state, quests: full_quests, party_quests: party_full_quests } });
+
 
   }
   return (
     <>
-    <NavEmpty />
-    <Container component="main" maxWidth="xs">
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Join a Lobby
+      <NavEmpty />
+      <Container component="main" maxWidth="xs">
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Join a Lobby
         </Typography>
-        <form className={classes.form} onSubmit={handleSubmit} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="join-lobby"
-            label="Lobby Code"
-            name="lobbyCode"
-            value={lobbyCode}
-            onChange={(e)=>setLobbyCode(e.target.value)}
-            autoComplete="123"
-            autoFocus
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}     
-          >
-            Join Lobby
+          <form className={classes.form} onSubmit={handleSubmit} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="join-lobby"
+              label="Lobby Code"
+              name="lobbyCode"
+              value={lobbyCode}
+              onChange={(e) => setLobbyCode(e.target.value)}
+              autoComplete="123"
+              autoFocus
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Join Lobby
           </Button>
           </form>
-          <form className={classes.form} onSubmit={handleSubmit}  noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="lobbyName"
-            value={lobbyName}
-            onChange={(e)=>setLobbyName(e.target.value)}
-            label="Lobby Name"
-            type="create-lobby"
-            id="create-lobby"
-            autoComplete="new-lobby"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Create New Lobby
+          <form className={classes.form} onSubmit={handleSubmit} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="lobbyName"
+              value={lobbyName}
+              onChange={(e) => setLobbyName(e.target.value)}
+              label="Lobby Name"
+              type="create-lobby"
+              id="create-lobby"
+              autoComplete="new-lobby"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Create New Lobby
           </Button>
-        </form>
-      </div>
-    </Container>
+          </form>
+        </div>
+      </Container>
     </>
   );
 }
