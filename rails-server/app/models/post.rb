baad5352a38c59f49ed8c5f0e6c55ea1ef70064d
Stoneface-Vimes <cliffweir92@gmail.com>
2020-03-17 20:09:12 -0400

    updated post table, now has a url column

--- rails-server/app/models/post.rb
+++ rails-server/app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   # :node must be singlular, since :nodes would refer to a belongs to many relationship
   belongs_to :node
   has_many :comments
-  VALIDATABLE_ATTRS = Post.attribute_names - %w(id created_at updated_at symbol_ref)
+  VALIDATABLE_ATTRS = Post.attribute_names - %w(id created_at updated_at symbol_ref url)
   validates_presence_of VALIDATABLE_ATTRS
 
 end
