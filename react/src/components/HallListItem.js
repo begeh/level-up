fa762337eb72f12811f3a62c68106d97de83d31d
ChristoLuksatrio <christo.luksatrio@gmail.com>
2020-03-23 13:18:55 -0400

    status indicator design add

--- react/src/components/HallListItem.js
+++ react/src/components/HallListItem.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Stepper, Step, StepLabel, Tooltip, Link} from '@material-ui/core';
+const classNames = require('classnames');
+
 
 
 export default function HallListItem({title, id, nodes, handleClick, mentor_id, user_id, status}){
+  const statusClass = classNames('status', {
+    'status-progress' : status === 'IN PROGRESS',
+    'status-success' : status === 'SUCCESS',
+    'status-failed' : status === 'FAILED'
+  })
 
   return (
     <Link 
@@ -11,8 +18,8 @@ export default function HallListItem({title, id, nodes, handleClick, mentor_id, 
       return handleClick(id, mentor_id, user_id)}} href=""
     >
     <div>
-      <h3 className='hall-title'>Quest Title: {title}</h3>
-      <p>Status: {status}</p>
+      <h3 className='hall-title'>{title}</h3>
+      <div className={statusClass}>{status}</div>
       <Stepper >
         {nodes.map((node, index) => {
   
