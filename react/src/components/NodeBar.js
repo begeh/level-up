6c9f8a72942be30c0830db24e34b0310229f3db1
ChristoLuksatrio <christo.luksatrio@gmail.com>
2020-03-26 15:06:49 -0400

    selected node highlight

--- react/src/components/NodeBar.js
+++ react/src/components/NodeBar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Grid, Stepper, Step, StepLabel, Tooltip } from '@material-ui/core';
 
+// className={selected_node === index ? 'selected-node' : null}
+
 export default function NodeBar({ nodes, handleNode, selected_node }) {
 
   return (
@@ -11,12 +13,15 @@ export default function NodeBar({ nodes, handleNode, selected_node }) {
 
           return (
             <Tooltip title={nodeInfo} arrow placement="bottom">
-              <Step style={selected_node === index ? {backgroundColor:"yellow"} : null} onClick={(event) => {
+              <Step onClick={(event) => {
                 event.preventDefault()
                 return handleNode(node.id, index)
               }}
                 key={index} >
-                <StepLabel className={node["is_complete?"] ? 'completed-node' : 'uncompleted-node'} />
+                <StepLabel className={
+                  (node["is_complete?"] && selected_node === index) ? 'completed-node selected-node'
+                  : node["is_complete?"] ? 'completed-node' 
+                  : 'uncompleted-node'} />
               </Step>
             </Tooltip>
           );
