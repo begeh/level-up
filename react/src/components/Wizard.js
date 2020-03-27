9d06093ec2d0d44600872cd897af15b964a765ff
Bashir <b_egeh@hotmail.com>
2020-03-27 15:41:23 -0400

    api call to hobbies list; new hobby in wizard quote box when wizard is clicked

--- react/src/components/Wizard.js
+++ react/src/components/Wizard.js
@@ -4,13 +4,13 @@ import wizard from '../images/wizard.png'
 
 
 
-export default function Wizard() {
+export default function Wizard({hobby, clickWizard}) {
   return (
     <Hidden xsDown>
     <div className='wizard-message'>
-      <p>Why don't you try learning kayaking?</p>
+      <p>Why don't you try learn about {hobby}?</p>
     </div>
-    <div className='wizard'>
+    <div onClick={clickWizard} className='wizard'>
       <img src={wizard} alt='wizards' />
     </div>
     </Hidden>
